bb185c982680e8b5b8ff0492c47c26622c2e1233
Lakshmy Venkitaraman <lakshmy.venkitaraman@terrificminds.com>
2025-08-15 09:53:00 +0530

    Made changes to layout

--- SkillMetrix/src/Layout/MainLayout.jsx
+++ SkillMetrix/src/Layout/MainLayout.jsx
@@ -4,19 +4,24 @@ import Header from '../Components/Header'
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen bg-gray-50 p-5">
-      <div className="flex h-screen">
-        <aside className="w-64 border-r bg-white">
-          <Sidebar />
-        </aside>
-        <main className="flex-1 flex flex-col">
-          <Header />
-          <div className="p-6 overflow-auto flex-1">
+    <div className="h-screen w-screen flex bg-gray-50 overflow-hidden fixed top-0 left-0">
+      {/* Sidebar */}
+      <aside className="w-64 bg-gray-100 border-r border-gray-200 flex-shrink-0 h-full ">
+        <Sidebar />
+      </aside>
+      
+      {/* Main Content Area */}
+      <main className="flex-1 flex flex-col h-full overflow-hidden">
+        {/* Header */}
+        <Header />
+        
+        {/* Main Content */}
+        <div className="flex-1 overflow-auto bg-gray-50">
+          <div className="p-6">
             <Outlet />
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   )
 }
-
